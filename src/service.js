e57bb4059625c2bc238c49dd5f827eec67759d0e
221Racheli <221racheli@gmaile.com>
2024-01-15 01:25:31 +0200

    first commit

---
+++ src/service.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://localhost:7170";
+
+axios.interceptors.response.use(function (response) {
+  console.log(`intercaption ${response.statusText}`);
+  return response;
+}, function (error) {
+  console.log(`intercaption ${error}`)
+  return Promise.reject(error);
+});
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get('/task')
+    return result.data;
+  },
+
+  addTask: async (name) => {
+    console.log('addTask', name)
+    const result = await axios.post('/task', { name: name, isComplete: "0" });
+    return result.data;
+  },
+
+  setCompleted: async (id, name, isComplete) => {
+    console.log('setCompleted', { id, name, isComplete });
+    const result = await axios.put('/task', { id, name, isComplete: Number(isComplete) });
+    return result.data;
+  },
+
+  deleteTask: async (id) => {
+    console.log('deleteTask -', { id });
+    await axios.delete(`/task/${id}`);
+  }
+};
